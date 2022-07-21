e64449fe152a1512f80b1f18a7cd802b07e5325d
LeeRJennings <leerjennings@outlook.com>
2022-07-21 10:04:45 -0500

    all models currently needed complete

---
+++ IfIFitz/Models/Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IfIFitz.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        public string ImageLocation { get; set; }
+
+        [Required]
+        public DateTime CreatedDateTime { get; set; }
+
+        [Required] 
+        public int SizeId { get; set; }
+
+        [Required] 
+        public int MaterialId { get; set; }
+
+        public UserProfile UserProfile { get; set; }
+
+        public Size Size { get; set; }
+
+        public Material Material { get; set; }
+    }
+}
--- IfIFitz/Models/UserProfile.cs
+++ IfIFitz/Models/UserProfile.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace IfIFitz.Models
 {
@@ -22,5 +23,7 @@ namespace IfIFitz.Models
         public string ImageLocation { get; set; }
         
         public bool IsActive { get; set; }
+
+        public List<Post> Posts { get; set; }
     }
 }
